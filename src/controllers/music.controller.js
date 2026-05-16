3a6c48c41220a418e67326b834d9164db4133025
Ankush Samanta <ankushsamanta2019@gmail.com>
2026-05-16 23:17:03 +0530

    linked it to imagekit fixed privatekey problem

--- src/controllers/music.controller.js
+++ src/controllers/music.controller.js
@@ -1,6 +1,6 @@
 const musicModel = require("../models/music.model");
 const jwt = require("jsonwebtoken");
-const uploadFile = require("../services/storage.service");
+const { uploadFile } = require("../services/storage.service");
 async function createMusic(req, res) {
   const token = req.cookies.token;
   if (!token) {
@@ -16,30 +16,31 @@ async function createMusic(req, res) {
             message: "Forbidden",
         });
     }
+  
+    const {title} = req.body;
+    const file = req.file;
+
+    const result = await uploadFile(file.buffer.toString("base64"));
+
+    const music = await musicModel.create({
+      uri: result.url,
+      title,
+      artist: decoded.id
+    })
+    res.status(201).json({
+      message: "Music created successfully",
+      music: {
+          id: music._id,
+          title: music.title,
+          uri: music.uri,
+          artist: music.artist
+      }
+    })
   } catch (err) {
     return res.status(401).json({
       message: "Unauthorized",
     });
   }
-  const {title} = req.body;
-  const file = req.file;
-
-  const result = await uploadFile(file.buffer.toString("base64"));
-
-  const music = await musicModel.create({
-    uri: result.url,
-    title,
-    artist: decoded.id
-  })
-  res.status(201).json({
-    message: "Music created successfully",
-    music: {
-        id: music._id,
-        title: music.title,
-        uri: music.uri,
-        artist: music.artist
-    }
-  })
 }
 
 module.exports = { createMusic }
