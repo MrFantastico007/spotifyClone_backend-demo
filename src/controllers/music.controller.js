48c578de17195f17d83a69f6172a0e7c6e04f87f
Ankush Samanta <ankushsamanta2019@gmail.com>
2026-05-16 23:01:55 +0530

    made create music api

--- src/controllers/music.controller.js
+++ src/controllers/music.controller.js
@@ -1,6 +1,6 @@
 const musicModel = require("../models/music.model");
 const jwt = require("jsonwebtoken");
-
+const uploadFile = require("../services/storage.service");
 async function createMusic(req, res) {
   const token = req.cookies.token;
   if (!token) {
@@ -21,4 +21,25 @@ async function createMusic(req, res) {
       message: "Unauthorized",
     });
   }
+  const {title} = req.body;
+  const file = req.file;
+
+  const result = await uploadFile(file.buffer.toString("base64"));
+
+  const music = await musicModel.create({
+    uri: result.url,
+    title,
+    artist: decoded.id
+  })
+  res.status(201).json({
+    message: "Music created successfully",
+    music: {
+        id: music._id,
+        title: music.title,
+        uri: music.uri,
+        artist: music.artist
+    }
+  })
 }
+
+module.exports = { createMusic }
